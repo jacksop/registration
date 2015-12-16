7d375336563abfc07fafbec7eaf9be5b4192baf8
jacksop <pjackson@mbs-projects.com>
2015-12-16 11:34:11 +0000

    Add react libraries

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+// super basic react component here
+
 $(document).ready(function() {
   $('#bio').keyup(function(event) {
     console.log('i changed');
     var count = $('#bio').val().length
     $('#counter').text(count)
   });
+  // render it once with some param
 });
