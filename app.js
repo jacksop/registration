3dc79a15ebbcc64649290d2cd9bc9c6fb696625a
jacksop <pjackson@mbs-projects.com>
2015-12-22 10:03:24 +0000

    Render the most basic of components with JSX

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-// super basic react component here
+var TextAreaWithCounter = React.createClass({
+  render: function render() {
+    return <p>
+      <label htmlFor="bio">Bio:</label>
+      <textarea id="bio"></textarea>
+      <span id="counter"></span>
+    </p>;
+  }
+});
 
 $(document).ready(function() {
+  ReactDOM.render(<TextAreaWithCounter />,
+    $('#bioInput')[0]);
+
+  /*
   $('#bio').keyup(function(event) {
     console.log('i changed');
     var count = $('#bio').val().length
     $('#counter').text(count)
   });
-  // render it once with some param
+  */
 });
+
